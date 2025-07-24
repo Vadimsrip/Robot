46b6bdf8b8b34f327439a901947a3bf9282402a3
Vadim Dolbilin <vadimdolbilin@yandex.ru>
2025-07-24 18:54:51 +0300

    Обновления

--- Robot.rb
+++ Robot.rb
@@ -3,7 +3,7 @@
 # Начало координат (0,0) можно считать самым юго-западным углом.
 # X-длинна , Y-ширина , F-направление робота
 
-  puts " Введите PLACE для запуска вашего робота.. "
+puts " Введите PLACE для запуска вашего робота.. "
 
 loop do
   name_komand = gets.to_s.strip.downcase
@@ -18,23 +18,23 @@ loop do
     puts "Первыя команда всегда должна быть PLACE , введите команду верно."
   end
 end
+
 puts "Введите команды.."
 
 koordinata_X = 0
 koordinata_Y = 0
 
-
-
 name_komand_1 = gets.to_s.strip.downcase.split
+
 argyments = name_komand_1[1].split(",")
- puts argyments.inspect 
+
 koordinata_X = argyments[0]
 koordinata_Y = argyments[1]
 koordinata_z = argyments[2]
 
 MOVE = [koordinata_X , koordinata_Y , koordinata_z]
 
-if name_komand_1 == "MOVE"
+if name_komand_1[0] == "MOVE"
   if koordinata_z == "Север"
     koordinata_Y = koordinata_Y + 1 
   elsif koordinata_z == "ЮГ" 
